c945e8f48b88387554947e3e854f60d49787e663
Lisandro Adagio <liadagio12@gmail.com>
2022-08-30 11:12:26 -0300

    Practica terminada

--- exercises/exercise9.py
+++ exercises/exercise9.py
@@ -32,17 +32,14 @@ domicilio = "Los sauces 3446"
 edad = "35"
 
 # COMPLETAR - INICIO
-conjunto_2 = set()
-conjunto_2.add(edad)
+conjunto_2 = {"5", "6", "7"}
 conjunto_2.add(nombre)
-conjunto_2.add(7)
-conjunto_2.add(6)
 conjunto_2.add(domicilio)
-conjunto_2.add(5)
+conjunto_2.add(edad)
 print(conjunto_2)
 # COMPLETAR - FIN
 
-assert conjunto_2 #== {"35", "Esteban", "7", "6", "Los sauces 3446", "5"}
+assert conjunto_2 == {"35", "Esteban", "7", "6", "Los sauces 3446", "5"}
 
 
 """
